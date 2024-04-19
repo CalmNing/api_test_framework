dcbeb27a3442ae37cfbfa7a8be9c699d3e068f8b
=anning <anning96@163.com>
2024-04-19 13:54:18 +0800

    生成文件中添加请求body

--- resolution.py
+++ resolution.py
@@ -3,11 +3,13 @@
 # @TIME    : 2024/4/16 13:11
 # @Author  : anning
 # @File    : resolution.py
+import json
 import os
 from functools import reduce
 from pathlib import Path
 
 import chevron
+import jsonpath
 import requests
 
 
@@ -60,6 +62,7 @@ class SwaggerJSONResolution:
                     if method_v.get('tags')[0] == file_name:
                         summary = method_v.get('summary', None)
                         parameters = method_v.get('parameters', None)
+                        requestBody = method_v.get('requestBody', None)
                         function_data = {
                             'function_name': uri.replace('/', '_').replace('-', '_')
                             .replace('{', '').replace('}', ''),
@@ -67,30 +70,64 @@ class SwaggerJSONResolution:
                             'uri': uri,
                             "summary": summary,
                             'method': method,
-                            'path': []
+                            'path': [],
+                            'requestBody': jsonpath.jsonpath(requestBody, '$.content.application/json.schema.$ref')
                         }
                         for parameter in parameters:
                             for key, value in parameter.items():
                                 if key == 'name' and value != "access-token":
                                     function_data.get("path").append(value)
                         class_data.get('interfaces').append(function_data)
+            # 获取所有接口的uri 并且用 / 分割成列表
             uri_list = []
             for interface in class_data.get('interfaces'):
                 uri_list.append(interface.get('uri').split('/'))
+            # 对多个接口uri取交集 拿到公共的部分 用来生成 类名称 和 文件名称
             sets = [set(lst) for lst in uri_list]
             intersection = reduce(lambda x, y: x & y, sets)
             result = list(intersection)
+            # 类名称首字符大写
             class_data['class_name'] = ''.join([element.capitalize() for element in result if element])
+            # 文件名称用下划线分割
             class_data['file_name'] = '_'.join([element for element in result if element])
             class_list.append(class_data)
         return class_list
+
+    def clen_data_body(self):
+        response = self.get_swagger_api_docs_response()
+        schemas = jsonpath.jsonpath(response, '$.components.schemas')
+        schema_list = []
+        for schema in schemas:
+            dict_a = {}
+            for model, model_v in schema.items():
+                dict_a[model] = {}
+                fields = jsonpath.jsonpath(model_v, f'$.properties')
+                for field in fields:
+                    for k, v in field.items():
+                        k_d = {}
+                        k_d[k] = v.get('type')
+                        dict_a[model].update(k_d)
+            schema_list.append(dict_a)
+        return schema_list
+
+    def generate_body(self):
+        data = self.clen_data()
+        for c in data:
+            for interface in c.get('interfaces'):
+                if interface.get('requestBody'):
+                    for model in self.clen_data_body():
+                        for k, v in model.items():
+                            if interface.get('requestBody')[0].split('/')[-1] == k:
+                                interface['body'] = v
+        return data
+
 
     def main(self):
         """
         生成接口文件
         :return:
         """
-        for class_l in self.clen_data():
+        for class_l in self.generate_body():
             with open('./interface.mustache', 'r') as mustache:
                 interfaces = chevron.render(mustache, class_l)
                 path = Path.cwd()
@@ -100,6 +137,7 @@ class SwaggerJSONResolution:
                 with open(case_demo_default, 'w', encoding='utf-8') as f:
                     f.write(interfaces)
 
+
 if __name__ == '__main__':
     sw = SwaggerJSONResolution('http://192.168.13.178:19983/luda-config/v3/api-docs')
-    class_list = sw.main()
+    print(json.dumps(sw.main()))
